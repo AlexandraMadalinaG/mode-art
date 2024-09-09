284aeaf324c4e9bb085c6757621a719ac5e59c06
AlexandraMadalinaG <madalinagrigore62@yahoo.com>
2024-09-09 18:30:59 +0100

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
                 <div className="nav-container flex1">
                     <nav className="flex1">
                         <div className="logo">
-                            <Link to="/">ModeArt</Link>
+                            <Link to="/" className='logo-link'>ModeArt</Link>
                         </div>
 
                         <ul
@@ -52,65 +52,4 @@ export default function Navbar() {
             </div>
         </>
     );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const Navbar = () => {
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-    const toggleMobileMenu = () => {
-        setIsMobileMenuOpen(!isMobileMenuOpen);
-    };
-
-    return (
-        <nav className="navbar">
-            <div className="nav-container">
-                <div className="logo">
-                    <Link to="/">ModeArt</Link>
-                </div>
-
-                {/*icon*/
-                /*<div className="menu-icon" onClick={toggleMobileMenu}>
-                    <i className={isMobileMenuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-
-                <ul className={isMobileMenuOpen ? 'nav-menu active' : 'nav-menu'}>
-                    <li className="nav-item">
-                        <Link to="/" className="nav-links" >
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-links">
-                            About
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/contact" className="nav-links">
-                            Contact
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/our-design" className="nav-links">
-                            Our Design
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
 };
-
-export default Navbar;*/
